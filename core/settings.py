e12b4d19249a1bc89d3f550702310cf454234cb6
shaha <shaha0111@mail.ru>
2022-02-10 23:42:35 +0600

    basic authorizations work

--- core/settings.py
+++ core/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'django_filters',
+    'rest_framework.authtoken',
     'rest_framework_simplejwt',
 
     'posts', 
