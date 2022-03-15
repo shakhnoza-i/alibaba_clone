00f26c908d6333b6f9465a84db4773d3bb2d176b
shaha <shaha0111@mail.ru>
2022-03-15 22:54:56 +0600

    image urlworks

--- core/core/urls.py
+++ core/core/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/', include('posts.urls')),
     path('account/', include('user_app.urls')),
     path('companies/', include('companies.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
